f9939d7d5901597c5a7fedb4e6f8b951c28f08c3
goluboleg06 <159870465+goluboleg06@users.noreply.github.com>
2024-09-25 08:14:25 +0300

    c

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string, string> metalsDict = new Dictionary<string, string>();
+            metalsDict.Add("Алюміній", "8.8");
+            metalsDict.Add("Залізо", "5.1");
+            metalsDict.Add("Титан", "0.6");
+            metalsDict.Add("Мідь", "0.1");
+
+            //Вивід елементів у прямому порядку
+            Console.WriteLine("Елементи у прямому порядку: ");
+            foreach (var item in metalsDict)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+            //Вивід елементів у зворотньому порядку
+            Console.WriteLine("Елементи у зворотньому порядку: ");
+            var reverseMetal = new List<KeyValuePair<string, string>>(metalsDict);
+            reverseMetal.Reverse();
+            foreach (var item in reverseMetal)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+            //Кількість елементів
+            Console.WriteLine($"Кількість елементів: {metalsDict.Count}");
+
+            //Очищення словника
+            metalsDict.Clear();
+            Console.WriteLine(metalsDict.Count);
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int min = Int32.MaxValue;
+            int max = Int32.MinValue;
+            int mult;
+
+            Console.WriteLine("К-ксть елементів: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1} елемент:");
+                stack.Push(Convert.ToInt32(Console.ReadLine()));
+            }
+            foreach (int num in stack)
+            {
+                if (min > num) min = num;
+                if (max < num) max = num;
+            }
+            mult = max * min;
+            Console.WriteLine($"Мінімальне значення: {min}");
+            Console.WriteLine($"Максимальне значення: {max}");
+            Console.WriteLine($"Добуток мінімального і максимального: {mult}");
+        }
+    }
+}
